25db755cc7606e5fba511470d5dc072bcb4f975d
Zahra <S170422@student.dtu.dk>
2021-03-10 21:30:24 +0100

    EmployeeDataAccess class completed

---
+++ DataAccess/EmployeeDataAccess.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Models;
+
+
+namespace DataAccess
+{
+    public class EmployeeDataAccess
+    {
+            public List<Employee> Employees { get; set; } = new List<Employee>();
+
+            public EmployeeDataAccess()
+            {
+                ReadEmployees();
+            }
+
+            private void ReadEmployees()
+            {
+                Employee emp1 = new Employee()
+                {
+                    Id = 1,
+                    FirstName = "George",
+                    LastName = "Orwell",
+                    PhoneNumber = 12121212,
+                    Address = "Tehran",
+                    Department = Department.Production,
+                    BaseSalary = 2500
+                };
+                Employee emp2 = new Employee()
+                {
+                    Id = 2,
+                    FirstName = "Aldous",
+                    LastName = "Huxley",
+                    PhoneNumber = 51234556,
+                    Address = "Tehran",
+                    Department = Department.Management,
+                    BaseSalary = 3500
+                };
+                Employees.Add(emp1);
+                Employees.Add(emp2);
+            }
+
+            public void AddEmployee(Employee emp)
+            {
+                Employees.Add(emp);
+            }
+
+            public void RemoveProduct(int id)
+            {
+                Employee temp = Employees.First(x => x.Id == id);
+                Employees.Remove(temp);
+            }
+
+            public void EditEmployee(Employee emp)
+            {
+                Employee temp = Employees.First(x => x.Id == emp.Id);
+                int index = Employees.IndexOf(temp);
+                Employees[index] = emp;
+            }
+
+            public int GetNexId()
+            {
+                int index = Employees.Any() ? Employees.Max(x => x.Id) + 1 : 1;
+                return index;
+            }
+        }
+    }
--- DataAccess/ProductDataAcces.cs
+++ DataAccess/ProductDataAcces.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using DataAccess.Models;
 
@@ -8,5 +9,56 @@ namespace DataAccess
     public class ProductDataAcces
     {
         public List<Product> Products { get; set; } = new List<Product>();
+
+        public ProductDataAcces()
+        {
+            ReadProducts();
+        }
+
+        private void ReadProducts()
+        {
+            Product pr1 = new Product()
+            {
+                Id = 1,
+                Name = "Animal Farm",
+                Author = "George Orwell",
+                AvailableCount = 12,
+                Price = 17
+            };
+            Product pr2 = new Product()
+            {
+                Id = 2,
+                Name = "Brave New World",
+                Author = "Aldous Huxley",
+                AvailableCount = 5,
+                Price = 34
+            };
+            Products.Add(pr1);
+            Products.Add(pr2);
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public void RemoveProduct(int id)
+        {
+            Product temp = Products.First(x => x.Id == id);
+            Products.Remove(temp);
+        }
+
+        public void EditProduct(Product product)
+        {
+            Product temp = Products.First(x => x.Id == product.Id);
+            int index = Products.IndexOf(temp);
+            Products[index] = product;
+        }
+
+        public int GetNexId()
+        {
+            int index = Products.Any()? Products.Max(x => x.Id) + 1 : 1;
+            return index;
+        }
     }
 }
